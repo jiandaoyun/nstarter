a0042b15c4021610c1b1a337c08faf4e3784831c
kyle <kyle@finereport.com>
2020-01-07 10:15:20 +0800

    update: FXD-8396 拆分 nstarter-core

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        "nstarter/node",
+        "nstarter/typescript",
+    ],
+    rules: {
+        "no-inner-declarations": "off",
+        "@typescript-eslint/no-namespace": "off"
+    }
+};
