b8c9b6082d2ecf949f17bc08a57f4794262180b2
James.Long <1181281178@qq.com>
2020-04-15 16:55:18 +0800

    update FXD-9402 消费者消息过期逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { AmqpConnector } from './lib/connector';
 export { IQueueConfig, RabbitMqQueue, queueFactory } from './lib/rabbitmq.queue';
 export { IQueueProducer, queueProducerFactory } from './lib/rabbitmq.producer';
-export { IQueueConsumer, queueConsumerFactory, startQueueConsumers } from './lib/rabbitmq.consumer';
+export { IQueueConsumer, IConsumerConfig, queueConsumerFactory, startQueueConsumers } from './lib/rabbitmq.consumer';
 
 export * from './constants';
 export * from './types';
