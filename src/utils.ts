b3b562382c323e04c625b9ad93d51543b4022780
kyle <kyle@finereport.com>
2020-08-07 15:14:47 +0800

    eslint

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,11 @@ export const once = (func: Function) => {
     let n = 2;
     return function (this: any, ...args: any[]) {
         if (--n > 0) {
-            result = fc.apply(this, args)
+            result = fc.apply(this, args);
         }
         if (n <= 1) {
-            fc = undefined
+            fc = undefined;
         }
         return result;
-    }
-}
+    };
+};
