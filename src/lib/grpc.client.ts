67b3c929a0c3cd370d003cc3aceaf9d8fc5c538c
Zed <Zed@fanruan.com>
2021-09-24 14:38:35 +0800

    update: FXD-18077 提供ssl配置

--- src/lib/grpc.client.ts
+++ src/lib/grpc.client.ts
@@ -1,16 +1,16 @@
-import grpc, { Client } from "grpc";
+import grpc, { Client } from 'grpc';
 
 import { IClientConfig } from '../types';
 import { getProtoServiceClient } from './proto';
 
-export const clientRegistry = new Map<string, string>();
+export const clientRegistry = new Map<string, IClientConfig>();
 
 /**
  * 注册 client 配置
  * @param conf
  */
 export const registerGrpcClientConfig = (conf: IClientConfig) => {
-    clientRegistry.set(conf.package, conf.address);
+    clientRegistry.set(conf.package, conf);
 };
 
 /**
@@ -20,8 +20,12 @@ export const registerGrpcClientConfig = (conf: IClientConfig) => {
  */
 export const getGrpcServiceClient = (pkg: string, service: string): Client => {
     const GrpcClient = getProtoServiceClient(pkg, service);
-    if (GrpcClient && clientRegistry.has(pkg)) {
-        return new GrpcClient(clientRegistry.get(pkg), grpc.credentials.createInsecure());
+    const clientConfig = clientRegistry.get(pkg);
+    if (GrpcClient && clientConfig) {
+        return new GrpcClient(
+            clientConfig.address,
+            clientConfig.useSsl ? grpc.credentials.createSsl() : grpc.credentials.createInsecure()
+        );
     } else {
         // 客户端未定义
         throw new Error(`Grpc client '${ pkg }.${ service }' not found.`);
