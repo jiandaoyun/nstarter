216a306d5958dcf488f4902e512debfd3ab76963
Zed <Zed@fanruan.com>
2021-09-27 17:31:34 +0800

    update: FXD-18077 主动获取服务端证书

--- src/lib/grpc.client.ts
+++ src/lib/grpc.client.ts
@@ -1,4 +1,6 @@
 import grpc, { Client } from 'grpc';
+import https, { Agent } from 'https';
+import { TLSSocket } from 'tls';
 
 import { IClientConfig } from '../types';
 import { getProtoServiceClient } from './proto';
@@ -18,16 +20,65 @@ export const registerGrpcClientConfig = (conf: IClientConfig) => {
  * @param pkg
  * @param service
  */
-export const getGrpcServiceClient = (pkg: string, service: string): Client => {
+export const getGrpcServiceClient = async (pkg: string, service: string): Promise<Client> => {
     const GrpcClient = getProtoServiceClient(pkg, service);
     const clientConfig = clientRegistry.get(pkg);
     if (GrpcClient && clientConfig) {
-        return new GrpcClient(
-            clientConfig.address,
-            clientConfig.useSsl ? grpc.credentials.createSsl() : grpc.credentials.createInsecure()
-        );
+        // 使用ssl
+        if (clientConfig.useSsl) {
+            // 泛域名证书
+            if (clientConfig.servername) {
+                const buffer = await getCertificateBuffer(clientConfig.address, clientConfig.servername!);
+                return new GrpcClient(clientConfig.address, grpc.credentials.createSsl(buffer), { 'grpc.ssl_target_name_override': clientConfig.servername! });
+            } else {
+                return new GrpcClient(clientConfig.address, grpc.credentials.createSsl());
+            }
+        } else {
+            return new GrpcClient(clientConfig.address, grpc.credentials.createInsecure());
+        }
     } else {
         // 客户端未定义
         throw new Error(`Grpc client '${ pkg }.${ service }' not found.`);
     }
+};
+
+const getCertificateBuffer = async (address: string, servername: string): Promise<Buffer> => {
+    const agent = new Agent({
+        requestCert: true,
+        rejectUnauthorized: false,
+        servername
+    });
+    return new Promise((onResolve, onReject) => {
+        const req = https.get('https://' + address,
+            {
+                protocol: 'https:',
+                agent,
+                method: 'GET'
+            },
+            res => {
+                const socket = res.socket as TLSSocket;
+                // 获取格式化之后的证书内容
+                const certString = pemEncode(socket.getPeerCertificate().raw.toString('base64'), 64);
+                return onResolve(Buffer.from(certString));
+            }
+        );
+        req.on('error', err => {
+            return onReject(err);
+        });
+    });
+
+};
+
+const pemEncode = (str: string, n: number) => {
+    const ret = [];
+
+    for (let i = 1; i <= str.length; i++) {
+        ret.push(str[i - 1]);
+        const mod = i % n;
+        if (mod === 0) {
+            ret.push('\n');
+        }
+    }
+
+    return `-----BEGIN CERTIFICATE-----\n${ ret.join('') }\n-----END CERTIFICATE-----`;
 };
