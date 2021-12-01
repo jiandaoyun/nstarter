47c9d45fc133ce3eecac5dbc26051dc0ed38f314
kyle <kyle@finereport.com>
2021-12-01 10:26:09 +0800

    fix: FXD-22629 URL API 升级后的兼容新问题

--- src/middlewares/request.extension.ts
+++ src/middlewares/request.extension.ts
@@ -1,4 +1,3 @@
-import { URL } from 'url';
 import { v4 as uuidv4 } from 'uuid';
 import { RequestHandler } from 'express';
 
@@ -10,7 +9,7 @@ import { RequestHandler } from 'express';
  */
 export const requestExtensionMiddleware: RequestHandler = (req, res, next) => {
     // 请求完整路径
-    req.originalPath = new URL(req.originalUrl, req.baseUrl).pathname;
+    req.originalPath = req.baseUrl + req.path;
 
     // 生成请求 id
     // note: 如果代理层未传入，则生成独立 uuid
