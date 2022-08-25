82b6bfc0e6a82ddd2daf8c8223e33f98352684fe
Mars.Du <Mars.Du@fanruan.com>
2022-08-25 14:26:58 +0800

    update:FXD-35373 index里新增引用

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import * as Constants from './constants';
 
 import * as CommonUtils from './utils/common.utils';
 import * as FileUtils from './utils/file.utils';
+import * as DateUtils from './utils/date.utils';
+import * as EncodeUtils from './utils/encode.utils';
+import * as HashUtils from './utils/hash.utils';
+import * as NumberUtils from './utils/number.utils';
+import * as StringUtils from './utils/string.utils';
 
 const NsUtils = {
     ...Constants,
     ...CommonUtils,
-    ...FileUtils
+    ...FileUtils,
+    ...DateUtils,
+    ...EncodeUtils,
+    ...HashUtils,
+    ...NumberUtils,
+    ...StringUtils
 };
 
 export = NsUtils;
--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -9,6 +9,32 @@
 import { CorpId } from './types';
 
 /**
+ * 生成模糊匹配 标点符号的字符串
+ * @param {string} text - 待检查的字符串
+ * @return {string} - 替换后的字符串
+ * @description  模糊匹配的符号有中文的逗号、左右括号
+ * @author Avery.Lee
+ */
+export const convertToFuzzyMatchPunctuationRegex = (text: string): string => {
+    if (text) {
+        return '^' + text.replace(/([（），,\*\-\.\$\^\(\)\[\]\{\}\|\+\?\\])/g, (matchStr) => {
+            if (matchStr === '(' || matchStr === '（') {
+                return '[（(]';
+            } else if (matchStr === ')' || matchStr === '）') {
+                return '[）)]';
+            } else if (matchStr === ',' || matchStr === '，') {
+                return '[，,]';
+            } else {
+                return '\\' + matchStr;
+            }
+        });
+    } else {
+        return '';
+    }
+};
+
+
+/**
  * 校验用户名是否合法
  * @param {string} val - 待校验用户名
  * @return {boolean} - 是否合法
