aab6cea9caa125c9ff8d706a26c1c82424cd5a6b
Mars.Du <Mars.Du@fanruan.com>
2022-08-26 09:27:45 +0800

    update:FXD-35373 去掉不常用方法

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import * as FileUtils from './utils/file.utils';
 import * as EncodeUtils from './utils/encode.utils';
 import * as HashUtils from './utils/hash.utils';
 import * as NumberUtils from './utils/number.utils';
-import * as StringUtils from './utils/string.utils';
 
 const NsUtils = {
     ...Constants,
@@ -13,8 +12,7 @@ const NsUtils = {
     ...FileUtils,
     ...EncodeUtils,
     ...HashUtils,
-    ...NumberUtils,
-    ...StringUtils
+    ...NumberUtils
 };
 
 export = NsUtils;
