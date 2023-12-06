1ec298a96accbb77c4a2c97942d4d6321ebfddee
Entropy <Entropy@fanruan.com>
2023-12-06 14:07:36 +0800

    update: FXD-52895 grpc原始抛错信息放到meta里面

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
 import { ServiceError } from '@grpc/grpc-js';
 import _ from 'lodash';
-import { NsError } from 'nstarter-core';
+import {
+    LogLevel,
+    NsError
+} from 'nstarter-core';
 
 /**
  * 首字母大写
@@ -30,19 +33,14 @@ export const getRpcName = (ctorName: string) => {
  * @param originErr
  */
 export const deserializeError = (originErr: ServiceError) => {
-    // GRPC 错误包装为 NsError
-    const err = new NsError('Grpc', 5);
-    // 从原始错误中获取错误码与错误信息
-    _.extend(err, {
-        errCode: _.toNumber(originErr.code),
-        errmsg: originErr.details,
-    });
     // 从原始错误中获取错误meta信息
     const meta = originErr.metadata?.getMap();
-    if (meta) {
-        _.extend(err, {
-            ...meta
-        });
-    }
-    return err;
+    // GRPC 错误包装为 NsError
+    return new NsError('Grpc', 5, LogLevel.error, {
+        meta: {
+            ...meta,
+            errcode: _.toNumber(originErr.code),
+            errmsg: originErr.details,
+        }
+    });
 };
